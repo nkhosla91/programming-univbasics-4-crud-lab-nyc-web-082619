d27ed322b27e9ba54cc887df78129fe78e8fd435
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-12 19:28:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,22 +21,19 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = []
-  narr = array.shift
-  return narr
+ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = [1,2,3,4]
-  p array(index_number)
+ array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
